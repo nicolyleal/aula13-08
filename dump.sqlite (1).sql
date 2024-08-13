86ca47637c45fb2f12542fda1da257c1eae8e0d0
nicolyleal <folkloresink@gmail.com>
2024-08-13 13:46:01 -0300

    Add files via upload

---
+++ dump.sqlite (1).sql
@@ -0,0 +1,16 @@
+-- TABLE
+CREATE TABLE alunos (
+  id int (3) UNIQUE,
+  nome varchar (30) NOT NULL,
+  idade varchar (2),
+  turma varchar (3)
+  
+  );
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
